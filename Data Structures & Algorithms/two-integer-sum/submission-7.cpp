2f3545e535e14bd7186ab2f8ffece93ebee1ee4e
ASIF HASAN TONMOY <92232794+asif-cse-kuet@users.noreply.github.com>
2026-08-31 21:15:21 +0600

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> f;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int d = target - nums[i];
+            if (f.contains(d)) return {f[d], i};
+            f[nums[i]]= i;
+        }
+        return{};
+    }
+};
