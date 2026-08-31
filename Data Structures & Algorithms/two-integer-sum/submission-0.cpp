fc5b076e9dbcf9e5ecd1826581cc61086332c468
ASIF HASAN TONMOY <92232794+asif-cse-kuet@users.noreply.github.com>
2026-08-31 20:58:22 +0600

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> freq;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (freq.find(target - nums[i]) != freq.end())
+            {
+                if(i<freq[target - nums[i]]){
+                    return {i, freq[target - nums[i]]};
+                }
+                else{
+                    return {freq[target - nums[i]], i};
+                }
+            }
+            else
+            {
+                freq[nums[i]]= i;
+            }
+        }
+    }
+};
